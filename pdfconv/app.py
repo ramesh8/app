f34966ec82b150a1e402bd8916c16ce3df26c7ab
Raja Ramesh Haddubangi <h.rajaramesh@gmail.com>
2023-11-24 19:22:01 +0530

    popen

--- pdfconv/app.py
+++ pdfconv/app.py
@@ -19,15 +19,31 @@ try:
     with open("go.docx", "rb") as fp:
         file_body = fp.read()
 
-    input_stream = io.BytesIO(file_body)
-    output_stream = io.BytesIO()
+    # input_stream = io.BytesIO(file_body)
+    # output_stream = io.BytesIO()
     # Run the LibreOffice command and capture stdout
-    process = subprocess.run(
-        libreoffice_command, input=input_stream.read(), check=True, capture_output=True
+    # process = subprocess.run(
+    #     libreoffice_command, input=input_stream.read(), check=True, capture_output=True
+    # )
+    process = subprocess.Popen(
+        libreoffice_command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=False
     )
     # Get the converted PDF content from the output stream
     # process = subprocess.run(libreoffice_command, input=file_body, stdout=subprocess.PIPE, check=True,capture_output=True)
-    pdf_content = process.stdout
+    # pdf_content = process.stdout
+    output = b""
+    process.stdin.write(file_body)
+    while True:
+        buff = process.stdout.read()
+        if len(buff) > 0:
+            output += buff
+        else:
+            errormsg = process.poll()
+            if errormsg is not None:
+                break
+
+    with open("output.pdf", "wb") as fp:
+        fp.write(output)
 except subprocess.CalledProcessError as e:
     print(f"Error running LibreOffice command: {e}")
     pdf_content = b""
